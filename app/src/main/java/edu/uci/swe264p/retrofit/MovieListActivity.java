4344905df0455427e6650a17ff3ed0df01391e1e
CarolynLo <88188378+CarolynLo@users.noreply.github.com>
2022-03-04 18:27:38 -0800

    Update MovieListActivity.java

--- app/src/main/java/edu/uci/swe264p/retrofit/MovieListActivity.java
+++ app/src/main/java/edu/uci/swe264p/retrofit/MovieListActivity.java
@@ -1,14 +1,78 @@
 package edu.uci.swe264p.retrofit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.gson.JsonObject;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class MovieListActivity extends AppCompatActivity {
+    static final String TAG = MovieListActivity.class.getSimpleName();
+    static final String BASE_URL = "https://api.themoviedb.org/3/";
+    static Retrofit retrofit = null;
+    final static String API_KEY = "56073b1c215973cb3fdc8c4979dd0ae8";
+    private RecyclerView recyclerView;
+    List<Movie> MovieList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_list);
+
+        recyclerView = findViewById(R.id.rvMovieList);
+        recyclerView.setHasFixedSize(true);
+        connect();
     }
+
+    private void connect(){
+
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        MovieApiService movieApiService = retrofit.create(MovieApiService.class);
+        Call<TopRatedResponse> call = movieApiService.getTopMovie(API_KEY);
+        call.enqueue(new Callback<TopRatedResponse>() {
+            @Override
+            public void onResponse(Call<TopRatedResponse> call, Response<TopRatedResponse> response) {
+                MovieList= response.body().getResults();
+                for(int i=0; i < MovieList.size(); i++){
+                    recyclerView.setLayoutManager(new LinearLayoutManager(MovieListActivity.this));
+                    recyclerView.setAdapter(new MovieListAdapter(MovieList));
+                }
+            }
+            @Override
+            public void onFailure(Call<TopRatedResponse> call, Throwable throwable) {
+                Log.e(TAG, throwable.toString());
+            }
+        });
+
+    }
+
 }

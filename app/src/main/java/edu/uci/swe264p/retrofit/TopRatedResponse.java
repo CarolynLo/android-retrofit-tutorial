b37029074a240111cbfe75ce1c1b38946c2ebcb7
CarolynLo <88188378+CarolynLo@users.noreply.github.com>
2022-03-04 18:28:38 -0800

    Update TopRatedResponse.java

--- app/src/main/java/edu/uci/swe264p/retrofit/TopRatedResponse.java
+++ app/src/main/java/edu/uci/swe264p/retrofit/TopRatedResponse.java
@@ -1,4 +1,16 @@
 package edu.uci.swe264p.retrofit;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class TopRatedResponse {
+    @SerializedName("results")
+    private List<Movie> results;
+
+    public TopRatedResponse(List<Movie> results) {
+        this.results=results;
+    }
+
+    public List<Movie> getResults(){ return results; }
 }
